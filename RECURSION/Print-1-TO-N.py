9bb999f9bea2676bfe6812e044609c8beb8ac0e7
vishalmishra311 <vishalmishra4400@gmail.com>
2025-09-11 12:47:30 +0530

    msg

---
+++ RECURSION/Print-1-TO-N.py
@@ -0,0 +1,13 @@
+# print 1 to N
+
+def oneToN(number):
+    if number == userInput:
+        return 
+    else:
+        print ("NUMBER-->>",number)
+        oneToN(number+1)
+userInput = int(input("enter a last number till u want to print -->"))
+oneToN(0)
+
+        
+    
